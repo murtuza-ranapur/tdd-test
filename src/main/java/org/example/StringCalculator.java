773443ba9618f721ff78040dbe5c474abb8f6f2c
murtuza-ranapur <mranapurwala56@gmail.com>
2020-06-12 11:09:33 +0530

    Adds Step 2 handling multiple numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,19 +3,20 @@ package org.example;
 public class StringCalculator {
 
     /**
-     * Takes input in form of String. Adds upto two numbers.
+     * Takes input in form of String. Adds multiple numbers.
      * Sample input "1,2". Sequence of numbers separated by ",".
      *
      * @param numbers String sequence of numbers
      * @return integer sum of given numbers
      */
     public int add(String numbers) {
+        int sum = 0;
         if(numbers.isEmpty())
             return 0;
         String [] nums = numbers.split(",");
-        if(nums.length == 1){
-            return Integer.parseInt(nums[0]);
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
         }
-        return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+        return sum;
     }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -16,23 +16,31 @@ class StringCalculatorTest {
     }
 
     @Test
-    public void add_emptyInput_pass(){
+    public void add_emptyInput_positive(){
         int expected = 0;
         String input = "";
         assertEquals(expected, stringCalculator.add(input));
     }
 
     @Test
-    public void add_singleInput_pass(){
+    public void add_singleInput_positive(){
         int expected = 1;
         String input = "1";
         assertEquals(expected, stringCalculator.add(input));
     }
 
     @Test
-    public void add_twoInput_pass(){
+    public void add_twoInput_positive(){
         int expected = 3;
         String input = "1,2";
         assertEquals(expected, stringCalculator.add(input));
     }
+
+    @Test
+    public void add_multiInput_positive(){
+        int expected = 10;
+        String input = "1,2,3,4";
+        assertEquals(expected, stringCalculator.add(input));
+    }
+    
 }
